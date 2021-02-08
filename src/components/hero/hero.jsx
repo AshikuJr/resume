5709b20f405728a8bad8ceff3389f33ea561b5d5
AshikuJr <ashikujr@gmail.com>
2021-02-09 00:09:47 +0700

    вторая попытка

--- src/components/hero/hero.jsx
+++ src/components/hero/hero.jsx
@@ -2,10 +2,12 @@ import React from 'react';
 import './hero.sass';
 
 const Hero = props => {
-  return <section className="hero">
-    <div className="hero__bg"></div>
-    <h1 className="hero__title">некит дебик </h1>
+  return <>
+  <div className="hero__bg"></div> 
+  <section className="hero">
+    <h1 className="hero__title">некит пидар</h1>
   </section>
+  </>
 };
 
 export default Hero;
