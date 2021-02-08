cea5b98b65d58a6377ab0e41bd5038a745c6f353
AshikuJr <ashikujr@gmail.com>
2021-02-08 22:29:43 +0700

    test fona

--- src/components/hero/hero.jsx
+++ src/components/hero/hero.jsx
@@ -1,7 +1,18 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 import './hero.sass';
 
 const Hero = props => {
+  function resize(){
+    let hero = document.querySelector('.hero');
+    hero.style.backgroundSize = `auto ${document.clientHeight}px`;
+  }
+  useEffect(()=>{
+    resize();
+    document.addEventListener('resize', resize);
+    return () => {
+      document.removeEventListener('resize', resize);
+    }
+  })
   return <section className="hero">
   </section>
 };
