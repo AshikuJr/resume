ddca66acfd84cd949cffcae79dd8ac8de7d1243d
AshikuJr <ashikujr@gmail.com>
2021-02-18 21:03:02 +0700

    контакты с нерабочей формой

--- src/components/contacts/contacts.jsx
+++ src/components/contacts/contacts.jsx
@@ -1,9 +1,71 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import './contacts.sass';
+
+import email from './email.png';
+import discord from './discord.png';
+import github from './github.png';
+
+const FormInput = props => {
+  let [value, setValue] = useState('');
+  function handleChange(event){
+    setValue(event.target.value);
+  };
+  function setFocus(event){
+    event.target.nextSibling.focus();
+  };
+  function moveLabel(event){
+    (value == '')&&event.target.previousSibling.classList.toggle('form__label_active');
+  };
+  return <div className="form-short" style={{gridArea: props.name}}>
+    <label htmlFor = {props.name} className = 'form__label' onClick={setFocus}>{props.label}</label>
+    <input type={props.type} name = {props.name} className="form__input" value={value} onChange = {handleChange} onFocus = {moveLabel} onBlur= {moveLabel}/>
+  </div>
+};
+
+const FormArea = props => {
+  let [value, setValue] = useState('');
+  function handleChange(event){
+    setValue(event.target.value);
+  };
+  function setFocus(event){
+    event.target.nextSibling.focus();
+  };
+  function moveLabel(event){
+    (value == '')&&event.target.previousSibling.classList.toggle('form__label_active');
+  };
+  return <div className="form-area" style={{gridArea: props.name}}>
+    <label htmlFor = {props.name} className = 'form__label' onClick={setFocus}>{props.label}</label>
+    <textarea type="text" name = {props.name} className="form__textarea" value={value} onChange = {handleChange} onFocus = {moveLabel} onBlur= {moveLabel}/>
+  </div>
+};
+
+const Account = props => {
+  return <div className="account">
+    <img src={props.icon} alt="" className="account__icon"/>
+    <span className="account__info">{props.info}</span>
+  </div>
+};
 
 const Contacts = props => {
   return <footer className="contacts" id='contacts'>
-
+    <div className="container">
+      <div className="contacts-wrap">
+        <div className="contacts-rest">
+          <h2 className="contacts__title">контакты</h2>
+          <Account icon = {email} info = 'AshikuJr@gmail.com'/>
+          <Account icon = {discord} info = 'AshikuJr#0299'/>
+          <Account icon = {github} info = 'github.com/AshikuJr'/>
+        </div>
+        <form className='form' action="#" method='post' name='feedback'>
+          <h3 className="form__title">Обратная связь</h3>
+          <FormInput name='name' type='text' label='Ваше имя'/>
+          <FormInput name='theme' type='text' label='Тема сообщения'/>
+          <FormArea name='comment' label='Сообщение'/>
+          <FormInput name='email' type='email' label='Ваша почта'/>
+          <input type="submit" className="form-button"/>
+        </form>
+      </div>
+    </div>
   </footer>
 };
 
--- src/components/hero/hero.jsx
+++ src/components/hero/hero.jsx
@@ -1,11 +1,28 @@
-import React, {useEffect} from 'react';
+import React, {useEffect, useState} from 'react';
 import './hero.sass';
 
+import bg from './full.jpg';
+
 const Hero = props => {
-  return <section className="hero">
+  let [ready, setReady] = useState(false);
+  async function preload(src){
+    await new Promise((resolve, reject) => {
+      const img = new Image();
+      img.src = src;
+      img.onload = resolve();
+      img.onerror = reject();
+    });
+    setReady(true);
+  };
+  useEffect(()=>{
+    preload(bg);
+  }, []);
+  return <>
+  {ready&&<section className="hero">
     <h1 className="hero__title">веб-разработчик</h1>
     <span className="hero__subtitle">резюме</span>
-  </section>
+  </section>}
+  </>
 };
 
 export default Hero;
